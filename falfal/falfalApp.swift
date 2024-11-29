360477629d745c001abe26bbc968795b60ba47b9
hasan-dogan <hasan.dogan@ideasoft.com.tr>
2024-11-29 13:21:08 +0300

    Initial Commit

---
+++ falfal/falfalApp.swift
@@ -0,0 +1,17 @@
+//
+//  falfalApp.swift
+//  falfal
+//
+//  Created by hasan doğan on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct falfalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ falfalTests/falfalTests.swift
@@ -0,0 +1,17 @@
+//
+//  falfalTests.swift
+//  falfalTests
+//
+//  Created by hasan doğan on 29.11.2024.
+//
+
+import Testing
+@testable import falfal
+
+struct falfalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
